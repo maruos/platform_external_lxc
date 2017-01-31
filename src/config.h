31324dd5112fb475bf6f21e78e931a6d4c28a0f4
Preetam D'Souza <preetamjdsouza@gmail.com>
2017-01-31 12:44:16 -0500

    Add prebuilt config.h from NDK 23
    This was generated using "./configure --host=arm-linux-androideabi" with
    a stand-alone toolchain from Android NDK API Level 23.
    
    This is probably a better idea than doing these by hand (you can see in
    the diff there were some missing some options previously).
    
    Signed-off-by: Preetam D'Souza <preetamjdsouza@gmail.com>

--- src/config.h
+++ src/config.h
@@ -1,54 +1,58 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
 /* src/config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Python3 is available */
-#undef ENABLE_PYTHON
+/* #undef ENABLE_PYTHON */
 
 /* Have cgmanager_get_pid_cgroup_abs_sync */
-#undef HAVE_CGMANAGER_GET_PID_CGROUP_ABS_SYNC
+/* #undef HAVE_CGMANAGER_GET_PID_CGROUP_ABS_SYNC */
+
+/* Have cgmanager_list_controllers */
+/* #undef HAVE_CGMANAGER_LIST_CONTROLLERS */
 
 /* Define to 1 if you have the `confstr' function. */
-#undef HAVE_CONFSTR
+/* #undef HAVE_CONFSTR */
 
 /* Define to 1 if you have the declaration of `PR_CAPBSET_DROP', and to 0 if
    you don't. */
-#undef HAVE_DECL_PR_CAPBSET_DROP
+#define HAVE_DECL_PR_CAPBSET_DROP 1
 
 /* Define to 1 if you have the declaration of
    `seccomp_syscall_resolve_name_arch', and to 0 if you don't. */
-#undef HAVE_DECL_SECCOMP_SYSCALL_RESOLVE_NAME_ARCH
+#define HAVE_DECL_SECCOMP_SYSCALL_RESOLVE_NAME_ARCH 0
 
 /* Define to 1 if you have the `endmntent' function. */
-#undef HAVE_ENDMNTENT
+#define HAVE_ENDMNTENT 1
 
 /* Define to 1 if you have the `faccessat' function. */
 #define HAVE_FACCESSAT 1
 
 /* Define to 1 if you have the `fgetln' function. */
-#undef HAVE_FGETLN
+#define HAVE_FGETLN 1
 
 /* Define to 1 if you have the `getline' function. */
-#undef HAVE_GETLINE
+#define HAVE_GETLINE 1
 
 /* Define to 1 if you have the `hasmntopt' function. */
-#undef HAVE_HASMNTOPT
+/* #undef HAVE_HASMNTOPT */
 
 /* Define to 1 if you have the <ifaddrs.h> header file. */
-#undef HAVE_IFADDRS_H
+/* #undef HAVE_IFADDRS_H */
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the `apparmor' library (-lapparmor). */
-#undef HAVE_LIBAPPARMOR
+/* #undef HAVE_LIBAPPARMOR */
 
 /* Define to 1 if you have the `pthread' library (-lpthread). */
-#undef HAVE_LIBPTHREAD
+/* #undef HAVE_LIBPTHREAD */
 
 /* Define to 1 if you have the `seccomp' library (-lseccomp). */
-#undef HAVE_LIBSECCOMP
+/* #undef HAVE_LIBSECCOMP */
 
 /* Define to 1 if you have the `util' library (-lutil). */
-#undef HAVE_LIBUTIL
+/* #undef HAVE_LIBUTIL */
 
 /* Define to 1 if you have the <linux/genetlink.h> header file. */
 #define HAVE_LINUX_GENETLINK_H 1
@@ -63,34 +67,34 @@
 #define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the `openpty' function. */
-#undef HAVE_OPENPTY
+#define HAVE_OPENPTY 1
 
 /* Define to 1 if you have the `pivot_root' function. */
-#undef HAVE_PIVOT_ROOT
+/* #undef HAVE_PIVOT_ROOT */
 
 /* Define to 1 if you have the `pthread_atfork' function. */
-#undef HAVE_PTHREAD_ATFORK
+#define HAVE_PTHREAD_ATFORK 1
 
 /* Define to 1 if you have the <pty.h> header file. */
-#undef HAVE_PTY_H
+/* #undef HAVE_PTY_H */
 
 /* Define to 1 if you have the `rand_r' function. */
-#undef HAVE_RAND_R
+#define HAVE_RAND_R 1
 
 /* Define to 1 if the system has the type `scmp_filter_ctx'. */
-#undef HAVE_SCMP_FILTER_CTX
+/* #undef HAVE_SCMP_FILTER_CTX */
 
 /* Define to 1 if you have the `sethostname' function. */
 #define HAVE_SETHOSTNAME 1
 
 /* Define to 1 if you have the `setmntent' function. */
-#undef HAVE_SETMNTENT
+#define HAVE_SETMNTENT 1
 
 /* Define to 1 if you have the `setns' function. */
 #define HAVE_SETNS 1
 
 /* Define to 1 if you have the `statvfs' function. */
-#undef HAVE_STATVFS
+#define HAVE_STATVFS 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
@@ -103,6 +107,9 @@
 
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/capability.h> header file. */
+/* #undef HAVE_SYS_CAPABILITY_H */
 
 /* Define to 1 if you have the <sys/personality.h> header file. */
 #define HAVE_SYS_PERSONALITY_H 1
@@ -120,7 +127,7 @@
 #define HAVE_SYS_TYPES_H 1
 
 /* Define if the compiler supports __thread */
-#undef HAVE_TLS
+/* #undef HAVE_TLS */
 
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
@@ -129,75 +136,75 @@
 #define HAVE_UNSHARE 1
 
 /* Define to 1 if you have the `utmpxname' function. */
-#undef HAVE_UTMPXNAME
+/* #undef HAVE_UTMPXNAME */
 
 /* Define to 1 if you have the <utmpx.h> header file. */
-#undef HAVE_UTMPX_H
+/* #undef HAVE_UTMPX_H */
 
 /* bionic libc */
 #define IS_BIONIC 1
 
 /* Enabling mutex debugging */
-#undef MUTEX_DEBUGGING
+/* #undef MUTEX_DEBUGGING */
 
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "lxc"
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "lxc"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "lxc 1.0.7"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "lxc"
 
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "1.0.7"
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* Enable extensions on AIX 3, Interix.  */
 #ifndef _ALL_SOURCE
-# undef _ALL_SOURCE
+# define _ALL_SOURCE 1
 #endif
 /* Enable GNU extensions on systems that have them.  */
 #ifndef _GNU_SOURCE
-# undef _GNU_SOURCE
+# define _GNU_SOURCE 1
 #endif
 /* Enable threading extensions on Solaris.  */
 #ifndef _POSIX_PTHREAD_SEMANTICS
-# undef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
 #endif
 /* Enable extensions on HP NonStop.  */
 #ifndef _TANDEM_SOURCE
-# undef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
 #endif
 /* Enable general extensions on Solaris.  */
 #ifndef __EXTENSIONS__
-# undef __EXTENSIONS__
+# define __EXTENSIONS__ 1
 #endif
 
 
 /* Version number of package */
-#undef VERSION
+#define VERSION "1.0.7"
 
 /* Define to 1 if on MINIX. */
-#undef _MINIX
+/* #undef _MINIX */
 
 /* Define to 2 if the system does not provide POSIX.1 features except with
    this defined. */
-#undef _POSIX_1_SOURCE
+/* #undef _POSIX_1_SOURCE */
 
 /* Define to 1 if you need to in order for `stat' and other things to work. */
-#undef _POSIX_SOURCE
+/* #undef _POSIX_SOURCE */
 
 /* Define to the compiler TLS keyword */
-#undef thread_local
+/* #undef thread_local */
